9cdecf37883be0dd958437626006f3b08b386a4f
Taha Emara <taha@emaraic.com>
2020-12-28 21:44:06 +0200

    Added faces clustering example

---
+++ examples/faces_clustering/Launcher.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author Taha Emara
+ * Website: http://www.emaraic.com
+ * Email  : taha@emaraic.com
+ * Created on: Dec 28, 2020 
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        FacesClustering.main(args);
+    }
+}
